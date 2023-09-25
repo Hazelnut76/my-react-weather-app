81abcac6f806bfab934bed548a23418ee096f224
Hazelnut76 <131495891+Hazelnut76@users.noreply.github.com>
2023-09-25 23:27:05 +0200

    Updated CSS, Weather, WeatherIcon and WeatherInfo components

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -2,29 +2,29 @@ import React from "react";
 import ReactAnimatedWeather from "react-animated-weather";
 
 export default function WeatherIcon(props) {
-    console.log(props.code);
-const codeMapping = {
-    "01d": "CLEAR_DAY",
-    "01n": "CLEAR_NIGHT",
-    "02d": "PARTLY_CLOUDS_DAY",
-    "02n": "PARTLY_CLOUDS_NIGHT",
-    "03d": "PARTLY_CLOUDS_DAY",
-    "03n": "PARTLY_CLOUDS_DAY",
-    "04d": "CLOUDS_DAY",
-    "04n": "CLOUDS_DAY",
-    "09d": "RAIN_DAY",
-    "09n": "RAIN_NIGHT",
-    "10d": "RAIN_DAY",
-    "10n": "RAIN_NIGHT",
-    "11d": "RAIN_DAY",
-    "11n": "RAIN_NIGHT",
-    "13d": "SNOW_DAY",
-    "13n": "SNOW_NIGHT",
-    "50d": "FOG_DAY",
-    "50n": "FOG_NIGHT",
-};
+ 
+  const codeMapping = {
+    "01d": "CLEAR",
+    "01n": "CLEAR",
+    "02d": "PARTLY_CLOUDS",
+    "02n": "PARTLY_CLOUDS",
+    "03d": "PARTLY_CLOUDS",
+    "03n": "PARTLY_CLOUDS",
+    "04d": "CLOUDS",
+    "04n": "CLOUDS",
+    "09d": "RAIN",
+    "09n": "RAIN",
+    "10d": "RAIN",
+    "10n": "RAIN",
+    "11d": "RAIN",
+    "11n": "RAIN",
+    "13d": "SNOW",
+    "13n": "SNOW",
+    "50d": "FOG",
+    "50n": "FOG",
+  };
 
-      return (
+  return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
       color="#eb6e4b"
@@ -32,5 +32,4 @@ const codeMapping = {
       animate={true}
     />
   );
-
-} 
+}
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -15,8 +15,8 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-            <WeatherIcon code={props.data.icon} />  
-                   
+          <WeatherIcon code={props.data.icon} />
+
           <WeatherTemperature celsius={props.data.temperature} />
         </div>
         <div className="col-6">
